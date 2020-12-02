eaabd8a367543ef4174f0ca2f6878ab3991fa53d
Christopher Perez <600rrchris@gmail.com>
2020-12-01 23:35:28 -0600

    product screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -5,8 +5,11 @@ const ProductScreen = {
   render: async () => {
   const request = parseRequestUrl();
   const product = await getProduct(request.id);
+  if(product.error){
+    return (`<div>${product.error}<div>`)
+  }
   return `<h1>${product.name}</h1>`
-  },
+},
 }
 
 export default ProductScreen;
